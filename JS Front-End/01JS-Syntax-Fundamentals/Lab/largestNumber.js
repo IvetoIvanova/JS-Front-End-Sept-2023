b4c591d4a7485bbd0e19933cb689bc20c49155b3
IvetoIvanova <135642648+IvetoIvanova@users.noreply.github.com>
2023-12-10 14:15:36 +0200

    Added solutions related to the 'JS Front-End' module.
    Added solutions in the 'JS Front-End' module of the "JS-Front-End-Sept-2023" course, with a focus on the JS-Syntax-Fundamentals. These solutions can be found in the 'Lab' folder of the '01JS-Syntax-Fundamentals' topic. The solutions themselves are named 'monthPrinter' , 'mathOperations' and 'largestNumber'.

---
+++ JS Front-End/01JS-Syntax-Fundamentals/Lab/largestNumber.js
@@ -0,0 +1,16 @@
+function printLargestNumber(num1, num2, num3) {
+    let result;
+
+    if (num1 > num2 && num1 > num3) {
+        result = num1;
+    } else if (num2 > num1 && num2 > num3) {
+        result = num2;
+    } else if (num3 > num1 && num3 > num2) {
+        result = num3;
+    }
+
+    console.log(`The largest number is ${result}.`);
+}
+
+printLargestNumber(5, -3, 16);
+printLargestNumber(-3, -5, -22.5);
