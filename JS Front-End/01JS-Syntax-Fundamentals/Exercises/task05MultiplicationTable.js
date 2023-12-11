bd50f5a369bb71c95bf8916874635f6ce42e629f
IvetoIvanova <135642648+IvetoIvanova@users.noreply.github.com>
2023-12-11 19:15:11 +0200

    Added solutions related to the 'JS Front-End' module.
    Added solutions in the 'JS Front-End' module of the "JS-Front-End-Sept-2023" course, with a focus on the JS-Syntax-Fundamentals. These solutions can be found in the 'Exercises' folder of the '01JS-Syntax-Fundamentals' topic. The solutions themselves are named 'task05MultiplicationTable' , 'task06SumDigits', 'task07CharsToString' and 'task08ReversedChars'.

---
+++ JS Front-End/01JS-Syntax-Fundamentals/Exercises/task05MultiplicationTable.js
@@ -0,0 +1,8 @@
+function multiplicationTable(x) {
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${x} X ${i} = ${x * i}`);
+    }
+}
+
+multiplicationTable(2);
+multiplicationTable(5);
---
+++ JS Front-End/01JS-Syntax-Fundamentals/Exercises/task08ReversedChars.js
@@ -0,0 +1,7 @@
+function reverseChars(a, b, c) {
+    const result = [c, b, a].join(' ');
+    console.log(result);
+}
+
+reverseChars('%', '2', 'o');
+
