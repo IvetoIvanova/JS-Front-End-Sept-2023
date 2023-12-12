2c8cc4a5d734410c10d8bc2f164406cf401b60b8
Ivelina Ivanova <135642648+IvetoIvanova@users.noreply.github.com>
2023-12-12 15:09:05 +0200

    Update task01ValidityChecker.js
    Deleted several lines with unnecessary comments that I wrote for ease and navigation during the solution.

--- JS Front-End/01JS-Syntax-Fundamentals/More Exercises/task01ValidityChecker.js
+++ JS Front-End/01JS-Syntax-Fundamentals/More Exercises/task01ValidityChecker.js
@@ -4,19 +4,14 @@ function validityChecker(x1, y1, x2, y2) {
         return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
     }
 
-    // Провери разстоянието от {x1, y1} до {0, 0}
     let distance1 = calculateDistance(x1, y1, 0, 0);
-    // Провери разстоянието от {x2, y2} до {0, 0}
     let distance2 = calculateDistance(x2, y2, 0, 0);
-    // Провери разстоянието от {x1, y1} до {x2, y2}
     let distance3 = calculateDistance(x1, y1, x2, y2);
 
-    // Провери дали разстоянията са цели числа
     let isValid1 = Number.isInteger(distance1);
     let isValid2 = Number.isInteger(distance2);
     let isValid3 = Number.isInteger(distance3);
 
-    // Отпечатай съобщение в зависимост от резултата
     if (isValid1) {
         console.log(`{${x1}, ${y1}} to {0, 0} is valid`);
     } else {
