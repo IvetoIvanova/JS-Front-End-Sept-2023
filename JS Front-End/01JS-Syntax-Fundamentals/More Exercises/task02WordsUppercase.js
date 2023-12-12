92f29492c446e22e392d3026bff28bebbcb7b08b
Ivelina Ivanova <135642648+IvetoIvanova@users.noreply.github.com>
2023-12-12 15:07:38 +0200

    Update task02WordsUppercase.js
    Deleted several lines with unnecessary comments that I wrote for ease and navigation during the solution.

--- JS Front-End/01JS-Syntax-Fundamentals/More Exercises/task02WordsUppercase.js
+++ JS Front-End/01JS-Syntax-Fundamentals/More Exercises/task02WordsUppercase.js
@@ -18,20 +18,16 @@ wordsUppercase('hello');
 
 
 function extractAndConvertToUpperCase(inputString) {
-    // Use a regular expression to match words (alphanumeric sequences)
     let words = inputString.match(/\w+/g);
 
     if (words) {
-        // Convert the words to uppercase
         let upperCaseWords = words.map(function (word) {
             return word.toUpperCase();
         });
 
-        // Join the uppercase words with ", "
         let result = upperCaseWords.join(", ");
 
         console.log(result);
-        //return result;
     } else {
         return "No words found in the input.";
     }
@@ -40,7 +36,3 @@ function extractAndConvertToUpperCase(inputString) {
 extractAndConvertToUpperCase('Hi, how are you?');
 extractAndConvertToUpperCase('hello');
 
-// Example usage:
-//   let inputString = "This is a sample sentence, with words that should be converted.";
-//   let convertedString = extractAndConvertToUpperCase(inputString);
-//   console.log(convertedString);
