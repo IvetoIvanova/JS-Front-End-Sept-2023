f5f3ae47a25653c05f9f0ef1ccf188c805217458
Ivelina Ivanova <135642648+IvetoIvanova@users.noreply.github.com>
2023-12-13 18:33:45 +0200

    Update task01SumFirstAndLastArrayElements.js
    Deleted unnecessary lines with comments in the code that I wrote for convenience during code composition.

--- JS Front-End/02JS-Arrays-and-Strings/Lab/task01SumFirstAndLastArrayElements.js
+++ JS Front-End/02JS-Arrays-and-Strings/Lab/task01SumFirstAndLastArrayElements.js
@@ -7,13 +7,3 @@ function sumFirstAndLastElements(inputArrOfNum) {
 sumFirstAndLastElements([20, 30, 40]);
 sumFirstAndLastElements([10, 17, 22, 33]);
 sumFirstAndLastElements([11, 58, 69]);
-
-// function sumFirstAndLastArrElements(inputArrOfNum) {
-//     let first = inputArrOfNum[0];
-//     let last = inputArrOfNum[inputArrOfNum.length - 1];
-//     console.log(first + last);
-// }
-
-// sumFirstAndLastArrElements([20, 30, 40]);
-// sumFirstAndLastArrElements([10, 17, 22, 33]);
-// sumFirstAndLastArrElements([11, 58, 69]);
