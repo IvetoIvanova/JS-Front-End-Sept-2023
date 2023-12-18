c367860ad60c024cbe2ec5a0dd0e43a3939db4f5
Ivelina Ivanova <135642648+IvetoIvanova@users.noreply.github.com>
2023-12-18 21:55:07 +0200

    Update getObjectCity.js
    Deleted unnecessary lines with comments in the code that I wrote for convenience during code composition.

--- JS Front-End/04JS-Objects-and-Classes/Lab/getObjectCity.js
+++ JS Front-End/04JS-Objects-and-Classes/Lab/getObjectCity.js
@@ -3,14 +3,6 @@ function getObject(object) {
         const [key, value] = entry;
         console.log(`${key} -> ${value}`);
     }
-
-    // const {name, area, population, country, postcode} = object;
-
-    // console.log(`name -> ${name}`)
-    // console.log(`area -> ${area}`)
-    // console.log(`population -> ${population}`)
-    // console.log(`country -> ${country}`)
-    // console.log(`postCode -> ${postCode}`)
 }
 
 getObject({
