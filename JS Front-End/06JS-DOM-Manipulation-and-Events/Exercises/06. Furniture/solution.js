3959f8af72dbe50e8c06e1fdd5f56096cb69b9df
IvetoIvanova <135642648+IvetoIvanova@users.noreply.github.com>
2023-12-23 00:28:23 +0200

    Added solutions related to the 'JS Front-End' module.
    Added  a solution for a task within the 'JS Front-End' module of the "JS-Front-End-Sept-2023" course, specifically addressing the JS DOM Manipulation and Events. The corresponding solution can be found in the 'Exercises' folder under the '06JS-DOM-Manipulation-and-Events' topic, with the solution folder named '06. Furniture'.

---
+++ JS Front-End/06JS-DOM-Manipulation-and-Events/Exercises/06. Furniture/solution.js
@@ -0,0 +1,82 @@
+function solve() {
+    const [generateTextArea, buyTextArea] = Array.from(document.getElementsByTagName('textarea'));
+    const [generateBtn, buyBtn] = Array.from(document.getElementsByTagName('button'));
+    const tbody = document.querySelector('.table > tbody');
+
+    generateBtn.addEventListener('click', generateHandler);
+    buyBtn.addEventListener('click', buyHandler);
+
+    function generateHandler() {
+        const data = JSON.parse(generateTextArea.value);
+
+        for (const { img, name, price, decFactor } of data) {
+            const tableRow = createElement('tr', '', tbody);
+            const firstColumnTd = createElement('td', '', tableRow);
+            createElement('img', '', firstColumnTd, '', '', { src: img });
+            const secondColumnTd = createElement('td', '', tableRow);
+            createElement('p', name, secondColumnTd);
+            const thirdColumnTd = createElement('td', '', tableRow);
+            createElement('p', price, thirdColumnTd);
+            const fourthColumnTd = createElement('td', '', tableRow);
+            createElement('p', decFactor, fourthColumnTd);
+            const fifthColumnTd = createElement('td', '', tableRow);
+            createElement('input', '', fifthColumnTd, '', '', { type: 'checkbox' });
+        }
+    }
+
+    function buyHandler() {
+        const allCheckedInputs = Array.from(document.querySelectorAll('tbody tr input:checked'));
+        let boughtItems = [];
+        let totalPrice = 0;
+        let totalDecFactor = 0;
+
+        for (const input of allCheckedInputs) {
+            const tableRow = input.parentElement.parentElement;
+            const [_firstColumn, secondColumn, thirdColumn, fourthColumn] = Array.from(tableRow.children);
+            let item = secondColumn.children[0].textContent;
+            boughtItems.push(item);
+            let currrentPrice = Number(thirdColumn.children[0].textContent);
+            totalPrice += currrentPrice;
+            let currentDecFactor = Number(fourthColumn.children[0].textContent);
+            totalDecFactor += currentDecFactor;
+        }
+
+        const averageDecFactor = totalDecFactor / allCheckedInputs.length;
+        buyTextArea.value += `Bought furniture: ${boughtItems.join(', ')}\n`;
+        buyTextArea.value += `Total price: ${totalPrice.toFixed(2)}\n`;
+        buyTextArea.value += `Average decoration factor: ${averageDecFactor}`;
+
+    }
+
+    function createElement(type, content, parentNode, id, classes, attributes) {
+        const htmlElement = document.createElement(type);
+
+        if (content && type !== 'input') {
+            htmlElement.textContent = content;
+        }
+
+        if (content && type === 'input') {
+            htmlElement.value = content;
+        }
+
+        if (id) {
+            htmlElement.id = id;
+        }
+
+        if (classes) {
+            htmlElement.classList.add(...classes);
+        }
+
+        if (attributes) {
+            for (const key in attributes) {
+                htmlElement.setAttribute(key, attributes[key]);
+            }
+        }
+
+        if (parentNode) {
+            parentNode.appendChild(htmlElement);
+        }
+
+        return htmlElement;
+    }
+}
