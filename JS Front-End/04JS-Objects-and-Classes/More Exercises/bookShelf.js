b5f837e2a2e6bdb4bf61fdebe28132062ec17e5b
Ivelina Ivanova <135642648+IvetoIvanova@users.noreply.github.com>
2023-12-29 10:54:56 +0200

    Update bookShelf.js
    Deleted unnecessary lines with comments in the code that I wrote for convenience during code composition.

--- JS Front-End/04JS-Objects-and-Classes/More Exercises/bookShelf.js
+++ JS Front-End/04JS-Objects-and-Classes/More Exercises/bookShelf.js
@@ -29,9 +29,6 @@ function storeBooks(input) {
         }
     }
 }
-
-storeBooks(['1 -> history', '1 -> action', 'Death in Time: Criss Bell, mystery', '2 -> mystery', '3 -> sci-fi',
-    'Child of Silver: Bruce Rich, mystery', 'Hurting Secrets: Dustin Bolt, action', 'Future of Dawn: Aiden Rose, sci-fi',
     
 storeBooks(['1 -> mystery', '2 -> sci-fi',
     'Child of Silver: Bruce Rich, mystery',
