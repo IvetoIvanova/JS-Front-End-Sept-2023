f79c88f9fe8b73bf4e7ddc54c4152a1a163820d3
IvetoIvanova <135642648+IvetoIvanova@users.noreply.github.com>
2023-12-29 10:53:06 +0200

    Added solution related to the 'JS Front-End' module.
    Added  a solution for a task within the 'JS Front-End' module of the "JS-Front-End-Sept-2023" course, specifically addressing the JS Objects and Classes. The corresponding solution can be found in the 'More Exercises' folder under the '04JS-Objects-and-Classes' topic, with the solution named 'bookShelf.js'.

---
+++ JS Front-End/04JS-Objects-and-Classes/More Exercises/bookShelf.js
@@ -0,0 +1,42 @@
+function storeBooks(input) {
+    const shelves = {};
+
+    for (const line of input) {
+        if (line.includes("->")) {
+            const [shelfId, shelfGenre] = line.split(" -> ");
+            if (!shelves.hasOwnProperty(shelfId)) {
+                shelves[shelfId] = { genre: shelfGenre, books: [] };
+            }
+        } else if (line.includes(":")) {
+            const [bookInfo, bookGenre] = line.split(", ").map((el) => el.trim());
+            const [bookTitle, bookAuthor] = bookInfo.split(":").map((el) => el.trim());
+
+            const shelf = Object.values(shelves).find((shelf) => shelf.genre === bookGenre);
+
+            if (shelf) {
+                shelf.books.push({ title: bookTitle, author: bookAuthor });
+            }
+        }
+    }
+
+    const sortedShelves = Object.entries(shelves).sort((a, b) => b[1].books.length - a[1].books.length);
+
+    for (const [shelfId, shelf] of sortedShelves) {
+        console.log(`${shelfId} ${shelf.genre}: ${shelf.books.length}`);
+        const sortedBooks = shelf.books.sort((a, b) => a.title.localeCompare(b.title));
+        for (const book of sortedBooks) {
+            console.log(`--> ${book.title}: ${book.author}`);
+        }
+    }
+}
+
+storeBooks(['1 -> history', '1 -> action', 'Death in Time: Criss Bell, mystery', '2 -> mystery', '3 -> sci-fi',
+    'Child of Silver: Bruce Rich, mystery', 'Hurting Secrets: Dustin Bolt, action', 'Future of Dawn: Aiden Rose, sci-fi',
+    'Lions and Rats: Gabe Roads, history', '2 -> romance', 'Effect of the Void: Shay B, romance',
+    'Losing Dreams: Gail Starr, sci-fi', 'Name of Earth: Jo Bell, sci-fi', 'Pilots of Stone: Brook Jay, history']);
+storeBooks(['1 -> mystery', '2 -> sci-fi',
+    'Child of Silver: Bruce Rich, mystery',
+    'Lions and Rats: Gabe Roads, history',
+    'Effect of the Void: Shay B, romance',
+    'Losing Dreams: Gail Starr, sci-fi',
+    'Name of Earth: Jo Bell, sci-fi']);
